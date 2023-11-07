927913413f7f9002d52f781313a81dfddbd7675b
amperstrand <amperstrand@localhost>
2023-11-07 09:45:00 +0100

    switched CustomRateProvider to use bitbank style endpoint because it supports bid/ask and multiple currencies instead of just one

--- BTCPayServer.Rating/Providers/CustomRateProvider.cs
+++ BTCPayServer.Rating/Providers/CustomRateProvider.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using BTCPayServer.Rating;
 using Newtonsoft.Json.Linq;
+
+
 
 namespace BTCPayServer.Services.Rates
 {
@@ -14,14 +18,30 @@ namespace BTCPayServer.Services.Rates
             _httpClient = httpClient ?? new HttpClient();
         }
 
-        public RateSourceInfo RateSourceInfo => new RateSourceInfo("custom", "Custom", "https://customrates.local/api/price?from_currency=BTC&to_currency=Bits");
+        public RateSourceInfo RateSourceInfo => new RateSourceInfo("custom", "Custom", "https://customrates.local/tickers");
 
         public async Task<PairRate[]> GetRatesAsync(CancellationToken cancellationToken)
         {
-            var response = await _httpClient.GetAsync("https://customrates.local/api/price?from_currency=BTC&to_currency=Bits", cancellationToken);
+            var response = await _httpClient.GetAsync("https://customrates.local/tickers", cancellationToken);
             var jobj = await response.Content.ReadAsAsync<JObject>(cancellationToken);
-            var value = jobj["public_price"]["to_price"].Value<decimal>();
-            return new[] { new PairRate(new CurrencyPair("BTC", "BITS"), new BidAsk(value)) };
+            var data = jobj.ContainsKey("data") ? jobj["data"] : null;
+            if (jobj["success"]?.Value<int>() != 1)
+            {
+                var errorCode = data is null ? "Unknown" : data["code"].Value<string>();
+                throw new Exception(
+                    $"BitBank Rates API Error: {errorCode}. See https://github.com/bitbankinc/bitbank-api-docs/blob/master/errors.md for more details.");
+            }
+            return ((data as JArray) ?? new JArray())
+                .Where(p => p["buy"].Type != JTokenType.Null && p["sell"].Type != JTokenType.Null)
+                .Select(item => new PairRate(CurrencyPair.Parse(item["pair"].ToString()), CreateBidAsk(item as JObject)))
+                .ToArray();
+        }
+        private static BidAsk CreateBidAsk(JObject o)
+        {
+            var buy = o["buy"].Value<decimal>();
+            var sell = o["sell"].Value<decimal>();
+            // Bug from their API (https://github.com/btcpayserver/btcpayserver/issues/741)
+            return buy < sell ? new BidAsk(buy, sell) : new BidAsk(sell, buy);
         }
     }
 }
